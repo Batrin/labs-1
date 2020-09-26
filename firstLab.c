6c31f9cbac3ebadfbe1faee86c607067886e94f5
oricadu <maksimova0130@gmail.com>
2020-09-26 11:22:08 +0700

    final version

--- firstLab.c
+++ firstLab.c
@@ -9,28 +9,19 @@ int main(int argc, char const *argv[])
 	int pid;
 	int status;
 
-	//fork();
-
 	switch(pid = fork())
 		{
 		  case -1: 
-		  	puts("error");/*при вызове fork() возникла ошибка*/
-		  	break;
+		  	perror(1);
 		  case 0: 
 		  	printf("descendant %d ", pid);
-		  	sleep(3);
 		  	printf("%s\n", argv[1]);
-		  	execvp(argv[1], argv);
+		  	execvp(argv[1], &argv[1]);
 		  	exit(0);
-		  	break;
 		  default: 
-		  	printf("parent %d status %d\n", pid, WEXITSTATUS(status));/*это код родительского процесса*/
-		  	sleep(3);
 		  	waitpid(pid, &status, 0);
+		  	printf("parent %d status %d\n", pid, WEXITSTATUS(status));
 		  	break;
 		}
-
-	//puts("Hello, World!");
-	//sleep(10);
 	return 0;
 }
