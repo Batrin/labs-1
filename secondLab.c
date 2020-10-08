36239ff91a6c31df9eba61e88151938ee22afa28
oricadu <maksimova0130@gmail.com>
2020-10-08 22:37:34 +0700

    update secondLab

--- secondLab.c
+++ secondLab.c
@@ -12,7 +12,7 @@ int main(int argc, char const *argv[])
 	char sym;
     int count = 0; 
 
-	int ch;
+	int ch, strLength = 0;
 	int num, i = 0, position = 0;
 
 	//file = fopen(name, "r");
@@ -20,34 +20,57 @@ int main(int argc, char const *argv[])
 	int file1;
 	file1 = open(name, O_RDONLY);
 	int arr[255];
+	int *lenghts = NULL;
+	int *positions = NULL;
+
 
 	char buff[64];
 
 	arr[0] = 0;
+	//lenghts = (int*)realloc(lenghts, 1 * sizeof(int));
+    
+    while(read(file1, buff, 1) != 0){
+    	//strLenght++;
+    	if (buff[0] == '\n')
+    	{
+    		//lenghts[i] = strLenght;
+    		i++;
+    		position = lseek(file1, 0, SEEK_CUR);
+    		//printf("\n|%d, %d|\n", i, position);
+    		positions = (int*)realloc(positions, (i) * sizeof(int));
+    		//lenghts = (int*)realloc(lenghts, (i) * sizeof(int));
+    		positions[i] = position;
+
+    		//strLenght = 0;
+    	}
+    	//printf("%s", buff);
+
+    }
     
 	do{
-		printf("Введите номер строки");
+		printf("Введите номер строки\n");
 		scanf("%d", &num);
-		lseek(file1, arr[num], SEEK_SET);
+		lseek(file1, positions[num-1], SEEK_SET);
+		strLength = positions[num] - positions[num-1];
+		read(file1, buff, strLength);
+		printf("\n%s\n", buff);
 
-	    while(read(file1, buff, 1) != 0){
-	    	if (buff[0] == '\n')
-	    	{
-	    		i++;
-	    		position = lseek(file1, 0, SEEK_CUR);
-	    		printf("\n|%d, %d|\n", i, position);
-	    		arr[i] = position;
-	    	}
-	    	printf("%s", buff);
-
-	    }
 	} while(num != 0);
 
-    printf("\n%d\n", arr[0]);
-    printf("%d\n", arr[1]);
-    printf("%d\n", arr[2]);
-    printf("%d\n", arr[3]);
+	printf("\n%d\n", positions[0]);
+	printf("%d\n", positions[1]);
+	printf("%d\n", positions[2]);
+	printf("%d\n", positions[3]);
+	printf("%d\n", positions[2] - positions[1]);
+	
 
+
+    /*printf("\n%d, %d\n", positions[0], lenghts[0]);
+    printf("\n%d, %d\n", positions[1], lenghts[1]);
+    printf("\n%d, %d\n", positions[2], lenghts[2]);
+    printf("\n%d, %d\n", positions[3], lenghts[3]);*/
+
+  
 
 
     //printf("Содержимое файла:%s\nВсего прочитано: %i\n", buff, count);
